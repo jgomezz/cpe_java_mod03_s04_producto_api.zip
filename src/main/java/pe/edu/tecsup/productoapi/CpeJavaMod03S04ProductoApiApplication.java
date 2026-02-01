95baf563e6acd905c256e031f8923f80cc077aaf
jgomezz <jgomezz@gmail.com>
2026-02-01 11:54:52 -0500

    first version

---
+++ src/main/java/pe/edu/tecsup/productoapi/CpeJavaMod03S04ProductoApiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.productoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpeJavaMod03S04ProductoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpeJavaMod03S04ProductoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/tecsup/productoapi/CpeJavaMod03S04ProductoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.productoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpeJavaMod03S04ProductoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
